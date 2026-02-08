f4efd10ecf3217c5191c55d84dc79b1565e419bf
thrddqno <antonio.dioquino@hotmail.com>
2026-02-09 04:48:52 +0800

    create DTOs

---
+++ 01-todo-list-api/src/main/java/com/thrddqno/todolist_api/domain/dto/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.thrddqno.todolist_api.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+	private String token;
+}
---
+++ 01-todo-list-api/src/main/java/com/thrddqno/todolist_api/domain/dto/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.thrddqno.todolist_api.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+	private String email;
+	private String password;
+}
