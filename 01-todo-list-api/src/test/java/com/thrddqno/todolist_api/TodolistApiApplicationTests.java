4adec9604b4246a67dd1932c50d3926e76e3d9fb
thrddqno <antonio.dioquino@hotmail.com>
2026-02-09 00:25:07 +0800

    Initialize "Todo List API" project

---
+++ 01-todo-list-api/src/test/java/com/thrddqno/todolist_api/TodolistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thrddqno.todolist_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
